84035256c93c169d3ce947ce82c51fe2e17f7122
Adam Clark <a.clark@comicrelief.com>
2017-06-09 23:54:50 +0100

    Add preact install to concourse test script

--- ci/concourse/npm-install-and-test.sh
+++ ci/concourse/npm-install-and-test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "Running npm install"
 npm install
+
+# NPM install Preact CLI globally
+echo "Installing preact CLI globally"
+npm i -g preact-cli
+npm install -g preact
+
 echo "Running npm test"
 npm run test
