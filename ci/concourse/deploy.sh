8b510cccca018c4464c3936e3b09d07fbd7048fa
Adam Clark <a.clark@comicrelief.com>
2017-07-05 16:18:21 +0100

    Add local configuration for CI deploy

--- ci/concourse/deploy.sh
+++ ci/concourse/deploy.sh
@@ -14,11 +14,9 @@ echo "Building contact form"
 preact build --no-prerender --production
 
 # Deploy to AWS s3 using aws-cli
-echo "Deploying to S3 bucket cr-projects-$3"
-export AWS_ACCESS_KEY_ID=$1
-export AWS_SECRET_ACCESS_KEY=$2
-aws s3 sync ./build s3://cr-projects-$3 --acl=public-read
+echo "Deploying to S3 bucket cr-projects-$DEPLOY_ENV"
+aws s3 sync ./build s3://cr-projects-$DEPLOY_ENV --acl=public-read
 
 # Remove the aws cli
 rm -rf ./aws-cli
-echo "Deployed site to http://cr-projects-$3.s3-website-eu-west-1.amazonaws.com"
+echo "Deployed site to http://cr-projects-$DEPLOY_ENV.s3-website-eu-west-1.amazonaws.com"
